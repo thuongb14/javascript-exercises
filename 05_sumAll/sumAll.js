6a77d7af029a4fc0c9c07b1c123fc19196c9f6ed
Lizzy <104043612+thuongb14@users.noreply.github.com>
2022-05-18 10:55:49 +1000

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,24 +3,23 @@ function sumAll(num1, num2) {
   if (typeof num1 != Number || typeof num2 != Number) {
     console.log('Error')
   } else {
-  
-  if (num1 < 0 || num2 < 0) {
-    console.log('Error')
-} else {
-  if (num1 < num2) {
-  for (i = num1; i <= num2; i++) {
-    sum = sum + i;
+    if (num1 < 0 || num2 < 0) {
+      console.log('Error')
+    } else {
+      
+    if (num1 < num2) {
+      for (i = num1; i <= num2; i++) {
+      sum = sum + i;
+      }
+    } else if (num1 > num2) {
+        for (i = num1; i >= num2; i--) {
+        sum = sum + i;
+        }
+      }
+      console.log(sum)
     }
-} else if (num1 > num2) {
-  for (i = num1; i >= num2; i--) {
-    sum = sum + i;
   }
 }
-  console.log(sum)
-}
-}
-}
-
 sumAll(10, [90, 1])
 };
 
